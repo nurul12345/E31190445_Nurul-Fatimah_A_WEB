2aa2f7472bc4ab8e9c62de3f8f4ab4854241a78b
nurul12345 <65608828+nurul12345@users.noreply.github.com>
2021-03-15 20:50:44 +0700

    upload

---
+++ tugas/minggu6/Helloview.php
@@ -0,0 +1,14 @@
+<html>
+<!--<head>Controller dan View</head>-->
+<head><title>Controller, Model dan View lebih dari 1 Variabel</title></head>
+<body>
+	<h2>Mengirim Data dari Controller ke View</h2>
+	
+	<!--memanggil variabel 1-->
+	variabel1: <?php echo variabel1; ?></br>
+	
+	<!--Memanggil variabel 2-->
+	variabel2: <?php echo variabel2; ?></br>
+	
+</body>
+</html>
