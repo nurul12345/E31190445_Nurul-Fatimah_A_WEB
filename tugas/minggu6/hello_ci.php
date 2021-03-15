71dc86f31cc062d80722a4b2393456ca45023a75
nurul12345 <65608828+nurul12345@users.noreply.github.com>
2021-03-15 20:54:07 +0700

    upload

---
+++ tugas/minggu6/hello_ci.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 	<title></title>
+</head>
+<body>
+	 <h1>Selamat Datang</h1>
+	 <p>Ini tampilan dari view hello_ci</p>
+	 <?=$nama;?>
+	 <?=$NIM?>
+	 <?=$Golongan;?>
+</body>
+</html>
