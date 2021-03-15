0648f653bee4e8f70398cd01c6a49a77f1cdaecd
nurul12345 <65608828+nurul12345@users.noreply.github.com>
2021-03-15 20:35:42 +0700

    upload

---
+++ tugas/minggu3/praktikum3.2.php
@@ -0,0 +1,20 @@
+<?php
+
+class Car {
+
+	//private
+	private $model;
+
+	public function getModel()
+	{
+		return "The car model is" . $this -> model;
+	}
+}
+
+$mercedes = new Car();
+
+////akses property dari luar class.
+$mercedes -> model = "Mercedes benz";
+echo $mercedes -> getModel();
+
+?>
